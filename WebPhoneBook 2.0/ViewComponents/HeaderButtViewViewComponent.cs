992f6d08143896f8dd83112f1ebfdaa0adcb7705
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-07-06 22:47:43 +0500

    компонент для отображения кнопок в шапке

---
+++ WebPhoneBook 2.0/ViewComponents/HeaderButtViewViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPhoneBook_2._0.ViewComponents
+{
+    public class HeaderButtViewViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
