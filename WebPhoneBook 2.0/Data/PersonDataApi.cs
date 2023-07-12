cf60abc12ad2431889d43547732d7e9f900f0abc
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-07-12 21:43:12 +0500

    начало работы над PersonDataApi

---
+++ WebPhoneBook 2.0/Data/PersonDataApi.cs
@@ -0,0 +1,18 @@
+﻿namespace WebPhoneBook_2._0.Data
+{
+    public class PersonDataApi : IPersonData
+    {
+        private HttpClient httpClient { get; set; }
+
+        public PersonDataApi()
+        {
+            httpClient = new HttpClient();
+        }
+
+            //public IEnumerable<Person> GetPeople()
+            //{
+            //    string url = @"https://localhost:44307/api/values";
+
+            //}
+    }
+}
